39e74f09b685d35664e64bf5321ca8d59168c314
Jakub Piotrowski <kokosek1139@gmail.com>
2015-11-25 18:49:23 +0100

    i think i understand devise

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem 'rails', '4.2.0'
 gem 'bcrypt'
+gem 'devise'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'coffee-script-source', '1.8.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
+  resources :posts
   resources :users
 
+root "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
