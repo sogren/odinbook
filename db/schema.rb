ef2e12d74ca6370ca003cd04688ba9565ae33c34
Sogren <kokosek1139@gmail.com>
2016-01-01 20:59:25 +0100

    test commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,52 +11,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_151_222_183_625) do
+ActiveRecord::Schema.define(version: 20151222183625) do
+
   create_table "comments", force: :cascade do |t|
-    t.text "content"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id"
-    t.integer "post_id"
+    t.integer  "author_id"
+    t.integer  "post_id"
   end
 
   create_table "friends_relations", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "friend_id"
+    t.integer  "user_id"
+    t.integer  "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "invitations", force: :cascade do |t|
-    t.integer "invited_user_id"
-    t.integer "inviting_user_id"
+    t.integer  "invited_user_id"
+    t.integer  "inviting_user_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.string "status"
-    t.string "message"
+    t.string   "status"
+    t.string   "message"
   end
 
-  add_index "invitations", %w(invited_user_id inviting_user_id), name: "index_invitations_on_invited_user_id_and_inviting_user_id", unique: true
+  add_index "invitations", ["invited_user_id", "inviting_user_id"], name: "index_invitations_on_invited_user_id_and_inviting_user_id", unique: true
   add_index "invitations", ["invited_user_id"], name: "index_invitations_on_invited_user_id"
   add_index "invitations", ["inviting_user_id"], name: "index_invitations_on_inviting_user_id"
 
   create_table "likes", force: :cascade do |t|
-    t.integer "likeable_id"
-    t.string "likeable_type"
+    t.integer  "likeable_id"
+    t.string   "likeable_type"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.integer "liker_id"
+    t.integer  "liker_id"
   end
 
-  add_index "likes", %w(likeable_id likeable_type liker_id), name: "likes_index", unique: true
+  add_index "likes", ["likeable_id", "likeable_type", "liker_id"], name: "likes_index", unique: true
   add_index "likes", ["liker_id"], name: "index_likes_on_liker_id"
 
   create_table "posts", force: :cascade do |t|
-    t.text "content"
+    t.text     "content"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer "author_id"
-    t.integer "receiver_id"
+    t.integer  "author_id"
+    t.integer  "receiver_id"
   end
 
   add_index "posts", ["receiver_id"], name: "index_posts_on_receiver_id"
@@ -64,37 +65,38 @@ ActiveRecord::Schema.define(version: 20_151_222_183_625) do
   create_table "profiles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.boolean "private"
+    t.integer  "user_id"
+    t.boolean  "private"
     t.datetime "birthday"
-    t.string "country"
-    t.text "about"
-    t.string "gender"
-    t.string "profession"
-    t.string "education"
+    t.string   "country"
+    t.text     "about"
+    t.string   "gender"
+    t.string   "profession"
+    t.string   "education"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string "email",                  default: "", null: false
-    t.string "encrypted_password",     default: "", null: false
-    t.string "reset_password_token"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.integer "avatar_file_size"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
 end
