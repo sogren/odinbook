3d3593fd84be5e3eadf2a6a9c3e432b188230cc6
Sogren <kokosek1139@gmail.com>
2016-04-07 16:31:07 +0200

    readme update

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Odinbook is facebook clone built in rails, it's the final project from The Odin 
 ### Features
 
 You can create your own account, make new friends, make posts on yours or on your friends timeline and comment all of it. To make new friends you need to invite them first and of course they need to accept your invitaiton, so you need to have link to their profile first. And you can upload your own profile picture and add some informaitons about you to your profile! Its almost as good as facebook! I only need to spend three more years on this and find 2 billions of users...
+
+
+Example account:
+email:     email1@example.com
+password:  qwerqwer
--- config/csrf_protection.rb
+++ config/csrf_protection.rb
@@ -2,11 +2,30 @@ class CsrfProtection
   def incoming(message, request, callback)
     session_token = request.session['_csrf_token']
     message_token = message['ext'] && message['ext'].delete('csrfToken')
+    user_id = request.session['warden.user.user.key'][0][0]
 
     unless true
       message['error'] = '401::Access denied'
     end
-
+    Rails.logger.debug "siema"
+    Rails.logger.debug session_token
+    Rails.logger.debug message_token
+    Rails.logger.debug message['error']
+    Rails.logger.debug message
+    Rails.logger.debug "reqruest"
+    Rails.logger.debug message['channel']
+    Rails.logger.debug "reqruest"
+    Rails.logger.debug request
+    Rails.logger.debug request.session
+    Rails.logger.debug user_id
+    Rails.logger.debug User.find(user_id)
+    Rails.logger.debug request.session.options
+    Rails.logger.debug request.session.values
+    Rails.logger.debug callback
+    Rails.logger.debug callback.call(message)
+    Rails.logger.debug
+    Rails.logger.debug User.all
+    Rails.logger.debug "hehe thats all"
     callback.call(message)
   end
 end
